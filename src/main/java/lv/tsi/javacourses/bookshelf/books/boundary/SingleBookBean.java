f882f213e198c23bb03abd26baf15d34d5646223
java_tsi08 <varturs81@gmail.com>
2019-01-10 19:30:19 +0200

    Bookshelf_vb1_10012019

---
+++ src/main/java/lv/tsi/javacourses/bookshelf/books/boundary/SingleBookBean.java
@@ -0,0 +1,36 @@
+package lv.tsi.javacourses.bookshelf.books.boundary;
+
+
+import lv.tsi.javacourses.bookshelf.books.model.BookEntity;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+
+@ViewScoped
+@Named
+
+public class SingleBookBean implements Serializable {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private Long id;
+
+    public BookEntity getBook(){
+
+        System.out.println("Opening book" + id);
+        return em.find(BookEntity.class,id);
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
